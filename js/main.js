745ad26e6537f4a34f3ab3751f6e25309a56bef4
coby_k <cokalter@gmail.com>
2024-04-25 17:24:55 -0400

    Added Earth img JS and initial CSS

---
+++ js/main.js
@@ -0,0 +1,7 @@
+const earthImg = document.createElement("img");
+const header = document.querySelector("header");
+
+
+earthImg.setAttribute("id", "earth-img");
+earthImg.setAttribute("src","img/earth-vector.png");
+header.appendChild(earthImg);
